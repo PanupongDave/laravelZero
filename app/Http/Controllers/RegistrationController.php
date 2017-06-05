0549a9649a94889563220098f20714972bbe4cb9
PanupongDave <panpongchmasomboon@gmail.com>
2017-06-05 23:34:07 +0700

    Add email

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Post;
+use App\Mail\Welcome;
 
 class RegistrationController extends Controller
 {
@@ -27,8 +28,9 @@ class RegistrationController extends Controller
             'password' => bcrypt(request('password'))
             ]);
 
-
     	auth()->login($user);
+
+        \Mail::to($user)->send(new Welcome($user));
 
     	return redirect()->home();
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 Route::get('/','PostsController@index')->name('home');
 Route::get('/posts/create','PostsController@create');
 Route::post('/posts','PostsController@store');
